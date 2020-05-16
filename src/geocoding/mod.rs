b48cef39b2aaf88da146088bfe1fee4c5afd329e
Donovan <awesomealpineibex@gmail.com>
2020-05-16 20:38:11 +0100

    read bytes not utf8

--- src/geocoding/mod.rs
+++ src/geocoding/mod.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 
-use csv::{Reader, StringRecord};
+use csv::{ByteRecord, Reader, StringRecord};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -35,32 +35,36 @@ pub enum GeocodingKind {
 pub fn search_coordinates(query: &str) -> String {
     let lat_index = 1;
     let lon_index = 2;
-    let res: StringRecord = build_geocoding_csv()
-        .records()
+    let res: ByteRecord = build_geocoding_csv()
+        .byte_records()
         .find(
             |record| record.as_ref()
-                .expect("Couldn't serialise record to a string record")
+                .expect("Couldn't serialise record to a byte record")
                 .iter()
-                .any(|field| field == query),
+                .any(|field| field == query.as_bytes()),
         )
         .unwrap_or_else(|| panic!("Unable to find coordinates for {}", query))
-        .expect("Issue unwrapping find");
-    format!("{};{}", res.get(lat_index).unwrap().to_owned(), res.get(lon_index).unwrap().to_owned())
+        .expect("Find result could not be unwrapped!");
+
+    format!("{};{}",
+            std::str::from_utf8(res.get(lat_index).unwrap().to_owned().as_ref()).expect("Unable to unwrap latitude"),
+            std::str::from_utf8(res.get(lon_index).unwrap().to_owned().as_ref()).expect("Unable to unwrap longitude")
+    )
 }
 
 pub fn search_postcode(lat_lon: Vec<f64>) -> String {
     let postcode_index = 0;
-    let res: StringRecord = build_geocoding_csv()
-        .records()
+    let res: ByteRecord = build_geocoding_csv()
+        .byte_records()
         .find(
             |record| record.as_ref()
-                .expect("Couldn't serialise record to a string record")
+                .expect("Couldn't serialise record to a byte record")
                 .iter()
-                .any(|field| field == lat_lon.first().unwrap().to_string()),
+                .any(|field| field == lat_lon.first().unwrap().to_string().as_bytes()),
         )
         .unwrap_or_else(|| panic!("Unable to find a postcode for {:?}", lat_lon))
-        .expect("Issue unwrapping find");
-    res.get(postcode_index).unwrap().to_owned()
+        .expect("Find result could not be unwrapped!");
+    String::from_utf8(res.get(postcode_index).unwrap().to_owned()).expect("Unable to unwrap postcode")
 }
 
 fn build_geocoding_csv() -> Reader<File> {
@@ -69,7 +73,7 @@ fn build_geocoding_csv() -> Reader<File> {
 
 #[cfg(test)]
 mod tests {
-    use crate::geocoding::{search_postcode, search_coordinates};
+    use crate::geocoding::{search_coordinates, search_postcode};
 
     #[test]
     fn test_search_postcode() {
@@ -83,5 +87,4 @@ mod tests {
         let coordinates = search_coordinates("AB1 0AJ");
         assert_eq!(coordinates, "57.099011;-2.252854")
     }
-
 }
