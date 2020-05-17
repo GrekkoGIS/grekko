64dfe9ffe55ba6ede7d6f4d8690144d72a933bb4
Donovan <awesomealpineibex@gmail.com>
2020-05-17 15:41:53 +0100

    rust doesnt always like chained function calls

--- tests/routes.rs
+++ tests/routes.rs
@@ -4,9 +4,16 @@ use warp::reply::Reply;
 
 #[tokio::test]
 async fn test_geocoding_forward() {
-    let result = grekko::receive_and_search_postcode(57.099011, -2.252854).await.unwrap().into_response().status();
-    let expected = warp::reply::json(&"AB1 0AJ");
-    assert_eq!(&result.as_str(), &expected.into_response().status().as_str())
+    let result = grekko::receive_and_search_postcode(57.099011, -2.252854).await.unwrap();
+    let result = result.into_response();
+    let result_status = result.status();
+    let result_status = result_status.as_str();
+
+    let expected = warp::reply::json(&"AB1 0AJ").into_response();
+    let expected_status = expected.status();
+    let expected_status = expected_status.as_str();
+
+    assert_eq!(&result_status, &expected_status)
 }
 
 #[tokio::test]
