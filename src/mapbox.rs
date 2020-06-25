c3b347292766d994aed744b8325b8bbfeee5b53b
Donovan <awesomealpineibex@gmail.com>
2020-06-25 20:47:45 +0100

    clean up the query

--- src/mapbox.rs
+++ src/mapbox.rs
@@ -1,5 +1,6 @@
+use std::env;
+
 use serde::{Deserialize, Serialize};
-use itertools::Itertools;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -27,27 +28,30 @@ pub struct Source {
 }
 
 pub async fn get_matrix(coordinates: Vec<Vec<f64>>) -> Option<Matrix> {
-    // let coords = coordinates.join(";");
-    // let coords = coords.join(",");
-
-    let crds: String = coordinates.iter().map(|c| {
-        let coords_cols_str: Vec<String> = c.iter().map(ToString::to_string).collect();
+    let coords: String = coordinates.iter().map(|long_lat| {
+        let coords_cols_str: Vec<String> = long_lat.iter().map(ToString::to_string).collect();
         coords_cols_str.join(",")
     })
         .collect::<Vec<String>>()
         .join(";");
 
+    let access_token = env::var("MAPBOX_ACCESS_KEY").expect("MAPBOX_ACCESS_KEY isn't set");
+
     let client = reqwest::Client::new();
-    let response_body = client.get("https://api.mapbox.com/directions-matrix/v1/mapbox/driving/-122.42,37.78;-122.45,37.91;-122.48,37.73")
+
+    let url = format!("https://api.mapbox.com/directions-matrix/v1/mapbox/driving/{}", coords.as_str());
+    let response_body = client.get(&url)
         .query(&[
-            ("access_token", ""),
+            ("access_token", access_token.as_str()),
             ("annotations", "distance")
         ])
         .send()
         .await.ok()?
         .text()
         .await.ok()?;
+
     let matrix: Matrix = serde_json::from_str(response_body.as_str()).ok()?;
+    println!("{:?}", matrix);
     Some(matrix)
 }
 
@@ -58,7 +62,12 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_matrix() {
-        let code = get_matrix(vec![vec![0.0, 0.0]]).await.unwrap().code;
+        let code = get_matrix(vec![
+            vec![-1.080278, 53.958332],
+            vec![-2.220000, 52.192001],
+            vec![-1.308000, 51.063202]
+        ])
+            .await.unwrap().code;
         assert_eq!(code, "Ok")
     }
 }
