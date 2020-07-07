c2bdeebd89e3d8c43d074def696d4cc414ed0063
Donovan <awesomealpineibex@gmail.com>
2020-07-07 21:22:11 +0100

    return matrix

--- src/osrm_service.rs
+++ src/osrm_service.rs
@@ -1,7 +1,7 @@
 use failure::Error;
 use osrm::{Coordinate, Osrm};
 
-pub fn table(coords: Vec<Vec<f32>>) -> Result<String, Error> {
+pub fn get_matrix(coords: Vec<Vec<f32>>) -> Result<Vec<Vec<f32>>, Error> {
     let osrm = Osrm::new("/Volumes/dev/osrm-data-ch/test-data/great-britain-latest.osrm")?;
 
     let destinations: Vec<Coordinate> = coords
@@ -11,14 +11,18 @@ pub fn table(coords: Vec<Vec<f32>>) -> Result<String, Error> {
             latitude: coord[1],
         })
         .collect();
-    build_source_array(&osrm, 2, &destinations)
+    let mut matrix: Vec<Vec<f32>> = vec![];
+    for (index, _) in destinations.clone().iter().enumerate() {
+        matrix.push(build_source_array(&osrm, index, &destinations)?);
+    }
+    Ok(matrix)
 }
 
 fn build_source_array(
     osrm: &Osrm,
     source_index: usize,
     destinations: &Vec<Coordinate>,
-) -> Result<String, failure::Error> {
+) -> Result<Vec<f32>, failure::Error> {
     let table = osrm.table(&*vec![destinations[source_index].clone()], &*destinations)?;
     let mut count = 1;
     let mut prior_count = 0;
@@ -32,21 +36,19 @@ fn build_source_array(
             count += 1;
             continue;
         } else {
-            println!(
+            log::trace!(
                 "Failed distance for {}, {}, ERROR: {:?}",
-                prior_count, count, result
+                prior_count,
+                count,
+                result
             );
             break;
         }
     }
-    let response = format!(
-        "OSRM Table response: duration: {:?}, distance: {:?}, {:?}",
-        table.get_duration(0, 1)?,
-        table.get_distance(0, 1)?,
-        durations,
-    );
-    log::debug!("{}", response);
-    Ok(response)
+
+    log::debug!("Durations for index `{}` is {:?}", source_index, durations);
+    println!("Durations for index `{}` is {:?}", source_index, durations);
+    Ok(durations)
 }
 
 #[cfg(test)]
@@ -55,28 +57,18 @@ mod tests {
 
     #[test]
     fn test_table() {
-        // [0,251.6,394.1],[258.2,0,371.4],[490.1,420.1,0]
-        // let destinations = vec![
-        //     Coordinate {
-        //         longitude: -2.242851,
-        //         latitude: 57.101474,
-        //     },
-        //     Coordinate {
-        //         longitude: -2.246308,
-        //         latitude: 57.102554,
-        //     },
-        //     Coordinate {
-        //         longitude: -2.248342,
-        //         latitude: 57.100556,
-        //     },
-        // ];
+        // [[0,108,149.1],[937.2,0,41.1],[896.1,33.9,0]]
+
+        // 0 108 149.1
+        // 937.2 0 41.1
+        // 896 33.9 0
         let coords = vec![
             vec![-2.242851, 57.101474],
             vec![-2.246308, 57.102554],
             vec![-2.248342, 57.100556],
         ];
 
-        let result = table(coords);
+        let result = get_matrix(coords);
         println!("{:?}", result);
         assert_eq!(result.is_err(), false)
     }
