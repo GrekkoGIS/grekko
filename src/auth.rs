860c27cf1410692db18363692b9162c6a2ac089c
Donovan <awesomealpineibex@gmail.com>
2020-07-11 13:16:22 +0100

    oops

--- src/auth.rs
+++ src/auth.rs
@@ -2,8 +2,11 @@ use alcoholic_jwt::{token_kid, validate, ValidJWT, Validation, JWKS};
 use failure::ResultExt;
 use jsonwebtoken::TokenData;
 use log::debug;
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
 
-struct Claims {
+#[derive(Deserialize)]
+pub struct Claims {
     pub uid: String,
     pub sub: String,
 }
